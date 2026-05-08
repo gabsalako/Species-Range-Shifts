6f7d4295f4835eeb52cb65f2d1ce53d286878f95
gabsalako <99962902+gabsalako@users.noreply.github.com>
2026-05-08 22:46:54 +0200

    Refactor RangeShifts script to handle NA values
    Removed NA values from Abundance and StackClimatex. Added initialization for aggregated data.

--- RangeModel/Rscripts/RangeShifts.1.R
+++ RangeModel/Rscripts/RangeShifts.1.R
@@ -8,7 +8,6 @@ Abundance <- rast(here("Data", "Abundance.tif"))
 TempC <- rast(here("Data", "Temp_Curr.tif")) / 10
 Temp50 <- rast(here("Data", "Temp_2050.tif"))
 Temp70 <- rast(here("Data", "Temp_2070.tif"))
-
 stackAll <-c(Abundance, TempC, Temp50, Temp70)
 #Check the alignmemnt of all raster
 stackAll
@@ -17,17 +16,21 @@ plot(stackAll)
 #Stack only Environmental carrying capacity (Climate data, Temperature)
 StackClimatex <-c(TempC, Temp50, Temp70)
 plot(StackClimatex)
-
-#aggregate to lower resolution to run faster
-Abundance_agg <- aggregate(Abundance, fact = 4)
-StackClimatex_agg <- aggregate(StackClimatex, fact = 4)
-
-#dealing wth NA less ideal
-Abundance_agg[is.na(Abundance_agg)] <- 0
-StackClimatex_agg[is.na(StackClimatex_agg)] <- 0
+# Remove NA
+Abundance <- na.omit(Abundance)#Remove NA completely
+StackClimatex <- na.omit(StackClimatex)#Remove NA completely
 
 #initialise
 sim_dataSF <- initialise(
+  n1_map =Abundance ,#represents NLmap
+  K_map =StackClimatex, # represents Kmap Lu can be 2006 or 2019 or climate scenarios
+  r = log(2),
+  rate = 1 / 1e3
+)
+#you may wish to aggregate to lower resolution to run faster
+Abundance_agg <- aggregate(Abundance, fact = 4)
+StackClimatex_agg <- aggregate(StackClimatex, fact = 4)
+sim_dataSF1 <- initialise(
   n1_map =Abundance_agg ,#represents NLmap
   K_map =StackClimatex_agg, # represents Kmap Lu can be 2006 or 2019 or climate scenarios
   r = log(2),
