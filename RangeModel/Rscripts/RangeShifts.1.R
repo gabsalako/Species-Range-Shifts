2d603f4e5c181cd3a8932e8599e08fce830708b9
gabsalako <99962902+gabsalako@users.noreply.github.com>
2026-05-09 19:19:17 +0200

    Update RangeShifts.1.R

--- RangeModel/Rscripts/RangeShifts.1.R
+++ RangeModel/Rscripts/RangeShifts.1.R
@@ -16,20 +16,24 @@ plot(stackAll)
 #Stack only Environmental carrying capacity (Climate data, Temperature)
 StackClimatex <-c(TempC, Temp50, Temp70)
 plot(StackClimatex)
-# Remove NA
-Abundance <- na.omit(Abundance)#Remove NA completely
-StackClimatex <- na.omit(StackClimatex)#Remove NA completely
-
+#Addressing NA in the grids
+#1. Create a master mask by summing everything
+master_mask <- Abundance + sum(StackClimatex)
+#2. Mask Abundance
+Abundance_clean <- mask(Abundance, master_mask)
+#3.Mask the entire climate layers
+#Using the master_mask ensures every layer in the climate matches abundance 
+StackClimatex_clean <- mask(StackClimatex, master_mask)
 #initialise
 sim_dataSF <- initialise(
-  n1_map =Abundance ,#represents NLmap
-  K_map =StackClimatex, # represents Kmap Lu can be 2006 or 2019 or climate scenarios
+  n1_map =Abundance_clean ,#represents NLmap
+  K_map =StackClimatex_clean, # represents Kmap Lu can be 2006 or 2019 or climate scenarios
   r = log(2),
   rate = 1 / 1e3
 )
 #you may wish to aggregate to lower resolution to run faster
-Abundance_agg <- aggregate(Abundance, fact = 4)
-StackClimatex_agg <- aggregate(StackClimatex, fact = 4)
+Abundance_agg <- aggregate(Abundance_clean, fact = 4)
+StackClimatex_agg <- aggregate(StackClimatex_clean, fact = 4)
 sim_dataSF1 <- initialise(
   n1_map =Abundance_agg ,#represents NLmap
   K_map =StackClimatex_agg, # represents Kmap Lu can be 2006 or 2019 or climate scenarios
@@ -42,7 +46,7 @@ summary(sim_dataSF)
 sim_result_SF <- sim(obj = sim_dataSF, time = 3)
 #use to plot the mean abundance graph at each time step
 summary(sim_result_SF)
-#plot the the predicted shift raster  at 3 time step
+#plot the the predicted shift raster  at 3 time steps
 #t_1=current t_1=2050, t_1=2070
 plot(sim_result_SF,
      time_points = c(1, 2, 3),
